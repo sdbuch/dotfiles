638194066aa497f315a10bd5dcb8df21d6cb8ed3
sdbuch <19997120+sdbuch@users.noreply.github.com>
2024-02-09 13:55:29 -0600

    minor hotkey updates

--- init.lua
+++ init.lua
@@ -925,7 +925,7 @@ local lazy_plugins = {
 					end, opts)
 					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
 					vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 					vim.keymap.set("n", "<space>cf", function()
 						vim.lsp.buf.format({ async = true })
