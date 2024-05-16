fad29bff30b8d743396cb753474e0b408d41d10c
sdbuch <19997120+sdbuch@users.noreply.github.com>
2024-05-16 14:19:38 -0400

    misc; update for new nvim

--- install_nvim_osx.sh
+++ install_nvim_osx.sh
@@ -5,14 +5,14 @@
 if [ ! -f /usr/local/bin/nvim ]; then
     # download
     # wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
-    wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
-    xattr -c ./nvim-macos.tar.gz
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos-arm64.tar.gz
+    xattr -c ./nvim-macos-arm64.tar.gz
     # extract
-    tar xzvf nvim-macos.tar.gz
+    tar xzvf nvim-macos-arm64.tar.gz
     # install
     sudo rm -rf /usr/local/nvim
-    sudo mv nvim-macos /usr/local/nvim
+    sudo mv nvim-macos-arm64 /usr/local/nvim
     sudo ln -s /usr/local/nvim/bin/nvim /usr/local/bin/nvim
     # cleanup
-    rm -f nvim-macos.tar.gz
+    rm -f nvim-macos-arm64.tar.gz
 fi
