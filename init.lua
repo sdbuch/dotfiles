de89cc8c2773b10f4351ce1a53d3c058e491ee72
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-07-24 14:05:03 -0500

    Revert "uv tool instead"
    This reverts commit b47c414c66796e05727be2668e5f011e33e9464d.

--- init.lua
+++ init.lua
@@ -1731,6 +1731,6 @@ require("lazy").setup({
 	},
 	-- for image.nvim + imagemagick luarocks install
 	rocks = {
-		hererocks = false, -- recommended if you do not have global installation of Lua 5.1.
+		hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
 	},
 })
